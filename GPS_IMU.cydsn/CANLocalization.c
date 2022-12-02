ab0e85b35f9606eac009ae524467d529e2f61dd6
unknown <minjang@uw.edu>
2022-12-01 18:47:19 -0800

    finally builds

--- GPS_IMU.cydsn/CANLocalization.c
+++
@@ -1,9 +0,0 @@
-/* File:         CANLocalization.c
- * Authors:      Jaden Bottemiller, Benton Kwong, Dylan Tomberlin.
- * Organization: Husky Robotics Team
- * 
- * This file includes fuction implementations for the Localization board CAN Communication
- * using the Hindsight CAN Communication standard.
- * Documentation: https://huskyroboticsteam.slite.com/app/channels/iU0BryG7M9/collections/aXvWTcIR6c/notes/4otlSFsSp2
- */
-#include "CANPacket.h"
